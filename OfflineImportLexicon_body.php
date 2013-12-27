fcaf76989016f1dbc095e54b79984c730403d3d6
Kunal Mehta <legoktm@gmail.com>
2013-12-26 23:13:52 -0800

    Replace usage of SpecialPage::getTitle with getPageTitle
    Was deprecated in 1.23, see Icdcf5d5295ef5e7f08b1d403e0c123f78738fd40
    
    Change-Id: I9ac03989e6f2e0dbdb3cd3b1edf119c3882c30f5

--- OfflineImportLexicon_body.php
+++ OfflineImportLexicon_body.php
@@ -21,7 +21,7 @@ class OfflineImportLexicon extends SpecialPage {
 
 	$this->setHeaders();
 	$this->outputHeader();
-    $selfTitle = $this->getTitle();
+    $selfTitle = $this->getPageTitle();
 	$wgOut->setPagetitle( wfMsg('offlineimportlexicon') );
     $actionUrl = $selfTitle->escapeLocalURL( );
 	$arrFormats = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
