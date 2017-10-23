bdb0f70abdb0bb37c35c0cba90ca0252b8ffb6ae
Umherirrender <umherirrender_de.wp@web.de>
2017-10-23 20:58:59 +0200

    Add missing qqq message documentation
    Activate banana checker
    
    Change-Id: I4d1afedda9bee81e29711dd5ed783800d2291e5e

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', /** 'banana' **/] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
